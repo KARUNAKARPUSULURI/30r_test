a7819f672abc5d90a21c2a6afd6b42d15a2bdaf5
KARUNAKARPUSULURI <karunpusuluri@gmail.com>
2025-04-07 16:52:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//modules -> esm -> types -> default modules, named 
+const a = 10
+const b = 20
+
+export default {a, b}
+
+export const obj = {
+    name : "karunakar",
+    age : 30,
+    gender : "male"
+}
+
+export const arr = [1,2,34,5,67,7]
---
+++ script.js
@@ -0,0 +1,5 @@
+import macha, { obj, arr } from "./index.js"
+console.log(macha.a)
+
+console.log(obj.name)
+// console.log(arr[0])
